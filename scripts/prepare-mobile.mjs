190c0281c7c0f41a03146807f448d1f150a4ec67
XandiF <34630424+farhadzandi@users.noreply.github.com>
2026-09-20 23:38:58 +0330

    build: include rollback module

--- scripts/prepare-mobile.mjs
+++ scripts/prepare-mobile.mjs
@@ -3,7 +3,7 @@ await mkdir('www', { recursive: true });
 for (const f of ['manifest.json','sw.js']) await copyFile(f, 'www/' + f);
 let html = await readFile('index.html','utf8');
 html = html.replace('</head>', '<link rel="stylesheet" href="mobile.css">\n</head>')
-           .replace('</body>', '<script src="platform.js"></script>\n<script src="mobile.js"></script>\n<script src="pages.js"></script>\n<script src="wizard.js"></script>\n<script src="onboarding.js"></script>\n<script src="settings.js"></script>\n<script src="data.js"></script>\n<script src="android.js"></script>\n</body>');
+           .replace('</body>', '<script src="platform.js"></script>\n<script src="mobile.js"></script>\n<script src="pages.js"></script>\n<script src="wizard.js"></script>\n<script src="onboarding.js"></script>\n<script src="settings.js"></script>\n<script src="data.js"></script>\n<script src="rollback.js"></script>\n<script src="android.js"></script>\n</body>');
 await writeFile('www/index.html', html);
-for (const f of ['mobile.css','platform.js','mobile.js','pages.js','wizard.js','onboarding.js','settings.js','data.js','android.js','rescue.html']) await copyFile('mobile/'+f,'www/'+f);
+for (const f of ['mobile.css','platform.js','mobile.js','pages.js','wizard.js','onboarding.js','settings.js','data.js','rollback.js','android.js','rescue.html']) await copyFile('mobile/'+f,'www/'+f);
 console.log('Ghestban Android 3.11 assets prepared in www/');
