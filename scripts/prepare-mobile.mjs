1a28bb61e87471bf4a0ed1886ac4c6c983b2079f
XandiF <34630424+farhadzandi@users.noreply.github.com>
2026-09-20 23:04:50 +0330

    build: inject 3.11 platform and pages

--- scripts/prepare-mobile.mjs
+++ scripts/prepare-mobile.mjs
@@ -3,15 +3,7 @@ await mkdir('www', { recursive: true });
 for (const f of ['manifest.json','sw.js']) await copyFile(f, 'www/' + f);
 let html = await readFile('index.html','utf8');
 html = html.replace('</head>', '<link rel="stylesheet" href="mobile.css">\n</head>')
-           .replace('</body>', '<script src="mobile.js"></script>\n<script src="wizard.js"></script>\n<script src="onboarding.js"></script>\n<script src="settings.js"></script>\n<script src="data.js"></script>\n<script src="android.js"></script>\n</body>');
+           .replace('</body>', '<script src="platform.js"></script>\n<script src="mobile.js"></script>\n<script src="pages.js"></script>\n<script src="wizard.js"></script>\n<script src="onboarding.js"></script>\n<script src="settings.js"></script>\n<script src="data.js"></script>\n<script src="android.js"></script>\n</body>');
 await writeFile('www/index.html', html);
-await copyFile('mobile/mobile.css','www/mobile.css');
-await copyFile('mobile/mobile.js','www/mobile.js');
-await copyFile('mobile/wizard.js','www/wizard.js');
-await copyFile('mobile/onboarding.js','www/onboarding.js');
-await copyFile('mobile/settings.js','www/settings.js');
-await copyFile('mobile/data.js','www/data.js');
-await copyFile('mobile/android.js','www/android.js');
-console.log('Ghestban Android assets prepared in www/');
-
-await copyFile('mobile/rescue.html','www/rescue.html');
+for (const f of ['mobile.css','platform.js','mobile.js','pages.js','wizard.js','onboarding.js','settings.js','data.js','android.js','rescue.html']) await copyFile('mobile/'+f,'www/'+f);
+console.log('Ghestban Android 3.11 assets prepared in www/');
